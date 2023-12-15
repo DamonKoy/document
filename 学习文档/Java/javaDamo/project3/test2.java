b5c0723927488a26b9a11fa3eb28893d48a123a2
冯志权 <admin@koydeMacBook-Pro.local>
2023-12-15 14:48:27 +0800

    调整课程3的练习

--- 学习文档/Java/javaDamo/project3/test2.java
+++ 学习文档/Java/javaDamo/project3/test2.java
@@ -1,4 +1,4 @@
-package project3;
+package ruqi.test.fengzhiquan.project3;
 
 import java.util.HashMap;
 import java.util.Scanner;
@@ -8,8 +8,9 @@ public class test2 {
     HashMap实现
      */
     public static class HashMapTest {
-        HashMap<String, String> info = new HashMap<>();
+        HashMap<String, SetProperty> info = new HashMap<>();
         Scanner input = new Scanner(System.in);
+
         public static void main(String[] args) {
         HashMapTest hashMapTest = new HashMapTest();
         hashMapTest.setInfo();
@@ -19,15 +20,24 @@ public class test2 {
         public void setInfo(){
 
             while (true) {
+                SetProperty setProperty = new SetProperty();
                 System.out.println("请输入员工姓名：“xxx”，输入“exit”为停止输入：");
                 String name = input.next();
                 //输入结束，退出登记簿编辑
                 if ("exit".equals(name)) {
                     break;
                 }
-                System.out.println("请输入员工信息（包括年龄、入职时间和员工状态）：");
-                String property = input.next();
-                info.put(name, property);
+                System.out.println("请输入员年龄：");
+                String age = input.next();
+                setProperty.setAge(Integer.parseInt(age));
+                System.out.println("请输入员工入职时间：");
+                String enterTime = input.next();
+                setProperty.setEnterTime(enterTime);
+                System.out.println("请输入员状态：");
+                String status = input.next();
+                setProperty.setStatus(status);
+                info.put(name, setProperty);
+                System.out.println(info);
             }
             //登记完成，把登记名称写入数据库
             System.out.println("登记完成，把登记名称存入数据库");
@@ -44,7 +54,7 @@ public class test2 {
          * @return 属性
          */
         public String getProperty(String name) {
-            return info.get(name);
+            return info.get(name).printInfo();
         }
     }
 
